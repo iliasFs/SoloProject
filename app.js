914547973466b3a30af6ba4da917733b88aad2bc
ilias <ilias.fasilakhs@gmail.com>
2022-09-26 22:34:24 +0300

    testimonials section + slide :-)

---
+++ app.js
@@ -0,0 +1,14 @@
+ 
+
+
+function moveDivRight() {
+
+    document.getElementById('slick__slide').style.transform = 'translateX(-50%)';
+
+}
+
+function moveDivLeft(){
+
+    document.getElementById('slick__slide').style.transform = 'translateX(0%)';
+
+}
