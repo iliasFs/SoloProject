61a928b0dec9c122547c9ba52eba913405189d7d
ilias <ilias.fasilakhs@gmail.com>
2022-09-27 20:46:05 +0300

    Pricing section Done

--- app.js
+++ app.js
@@ -12,3 +12,18 @@ function moveDivLeft(){
     document.getElementById('slick__slide').style.transform = 'translateX(0%)';
 
 }
+
+
+
+function arrowMove(){
+ 
+    document.getElementById('ans1').style.opacity = '1';
+    document.getElementById('ans1').style.backgroundColor = '#edf2f7';
+    document.getElementById('arrow__button').style.transform ='rotate(180deg)';
+}
+
+    
+
+
+
+
