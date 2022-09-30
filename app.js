7888ca6417413cb6fb9c6b5f171f542ade2f71b5
ilias <ilias.fasilakhs@gmail.com>
2022-09-30 19:46:59 +0300

    trying animation in JS

--- app.js
+++ app.js
@@ -12,14 +12,64 @@ function moveDivLeft(){
     document.getElementById('slick__slide').style.transform = 'translateX(0%)';
 
 }
+const qualitySection = document.getElementById('.quality');
+const stepsSection = document.querySelector('.steps');
+const valuesSection = document.querySelector('.values');
+const pricingSection = document.querySelector('.pricing');
+const testimonialSection =document.querySelector('.testimonial');
+const faqSection = document.querySelector('.FAQ');
+const footerSection = document.querySelector('.footer');
 
 
 
-function arrowMove(){
- 
-    document.getElementById('ans1').style.opacity = '1';
-    document.getElementById('ans1').style.backgroundColor = '#edf2f7';
-    document.getElementById('arrow__button').style.transform ='rotate(180deg)';
+document.addEventListener(scroll, ()=>{
+const clientHeight = document.documentElement.clientHeight;
+const qualitySectionY = qualitySection.getBoundingClientRect().y;
+const stepsSectionY = stepsSection.getBoundingClientRect().y;
+const valuesSectionY = valuesSection.getBoundingClientRect().y;
+const pricingSectionY = pricingSection.getBoundingClientRect().y;
+const testimonialSectionY = testimonialSection.getBoundingClientRect().y;
+const faqSectionY = faqSection.getBoundingClientRect().y;
+const footerSectionY = footerSection.getBoundingClientRect().y;
+
+
+
+
+if(clientHeight > qualitySectionY){
+
+    qualitySection.styles.animation = ('section-pricing 1s')
 }
+
+if(clientHeight > stepsSectionY){
+
+    stepsSection.styles.animation = ('section-pricing 2s')
+}
+if(clientHeight > valuesSectionY){
+
+    valuesSection.styles.animation = ('section-pricing 1s')
+}
+if(clientHeight > pricingSectionY){
+
+    pricingSection.styles.animation = ('section-pricing 2s')
+}
+if(clientHeight > testimonialSectionY){
+
+    testimonialSection.styles.animation = ('section-pricing 1s')
+}
+if(clientHeight > faqSectionY){
+
+    faqSection.styles.animation = ('section-pricing 2s')
+}
+if(clientHeight > footerSectionY){
+
+    footerSectionY.styles.animation = ('section-pricing 1s')
+}
+
+
+
+
+
+
+})
 
 
